23e196e9881a1e49fe40ab04d94f126ddd9f5e8a
ubuntu5678 <tl141315@cis.fukuoka-u.ac.jp>
2017-06-27 08:52:44 +0000

    Add

---
+++ scripts/wall_stop.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import rospy,copy
+from std_msgs.msg import Twist
+from std_srvs.srv import TriggerResponse
+from pimouse_ros.msg import LightSensorValues
+
+class WallStop()
+    def __init__(self):
+        self.cmd_vel = rospy.Publisher('/cmd_val',Twist,queue_size=1)
+
+        self.sensor_values = LightSensorValues()
+        rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
+
+    def callback(self,messages):
+        self.sensor_values = messages
+
+    def run(self):
+        rate = rospy.Rare(10)
+        data = Twist()
+
+        while not rospy.is_shutdown():
+            data.linear.x = 0.2 if self.sensor_values.sum_all <500 else 0.0
+            self.cmd_vel.publish(data)
+            rata.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('wall_stop')
+    rospy.wait_for_service('/motor_on')
+    rospy.wait_for_service('/motor_off')
+    rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
+    rospy.ServiceProxy('/motor_on',Trigger).call()
+    WallStop().run()
+
